8a4f6744acdf4a88aefa50e7432eda4ba8756f96
peyro <peyrovskaaa@gmail.com>
2025-09-25 17:02:31 +0200

    Set PYTHONPATH environment variable in Dockerfile

--- app/handlers/generate_invite_code.py
+++ app/handlers/generate_invite_code.py
@@ -7,29 +7,35 @@ from app.config import settings, Environment
 from app.database.crud.invite_code import InviteCodeService
 import secrets
 
+from app.database.crud.user import UserService
 from app.database.db.session import get_db
 from app.database.schemas.invite_code import InviteCodeCreate
 from app.services.hash_code import hash_secret
 
 invite_code_router = Router()
 
-if settings.ENVIRONMENT == Environment.DEVELOPMENT:
-    @invite_code_router.message(Command('generate_invite_code_dev'))
-    async def generate_invite_code_dev(message):
-        message_to_edit = await message.answer("Generating invite code...")
-        async with get_db() as db:
-            code = secrets.token_urlsafe(InviteCodeService.CODE_LENGTH)
-            hashed_code = hash_secret(code)
-            invite_code_service = InviteCodeService(db)
-            await invite_code_service.create(InviteCodeCreate(
-                code_hash=hashed_code,
-                expires_at=datetime.now(timezone.utc) + timedelta(seconds=InviteCodeService.CODE_EXPIRE_AFTER_SECONDS))
-            )
-            bot = message.bot
-            bot_user = await bot.get_me()
-            bot_username = bot_user.username
+@invite_code_router.message(Command('generate_invite_code_dev'))
+async def generate_invite_code_dev(message):
 
-            await message_to_edit.edit_text(f"Generated link: \n"
-                                            f"t.me/{bot_username}?start={code}\n"
-                                            f"Code expires after {round(InviteCodeService.CODE_EXPIRE_AFTER_SECONDS / 60)} minutes")
+    async with get_db() as db:
+        user_service = UserService(db)
+        user = await user_service.get_by_telegram_id(str(message.from_user.id))
+        if not user:
+            return
+
+        message_to_edit = await message.answer("Generating invite code...")
+        code = secrets.token_urlsafe(InviteCodeService.CODE_LENGTH)
+        hashed_code = hash_secret(code)
+        invite_code_service = InviteCodeService(db)
+        await invite_code_service.create(InviteCodeCreate(
+            code_hash=hashed_code,
+            expires_at=datetime.now(timezone.utc) + timedelta(seconds=InviteCodeService.CODE_EXPIRE_AFTER_SECONDS))
+        )
+        bot = message.bot
+        bot_user = await bot.get_me()
+        bot_username = bot_user.username
+
+        await message_to_edit.edit_text(f"Generated link: \n"
+                                        f"t.me/{bot_username}?start={code}\n"
+                                        f"Code expires after {round(InviteCodeService.CODE_EXPIRE_AFTER_SECONDS / 60)} minutes")
 
